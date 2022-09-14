18a36f965a19142b68e67b95da93657fbce3b02c
ewave99 <ehmurdoch47@protonmail.com>
2022-09-14 15:46:52 +0100

    added buttons panel

--- gui.c
+++ gui.c
@@ -35,6 +35,16 @@ activate(GtkApplication *app, gpointer user_data)
     gtk_main();
 }
 
+static GdkRectangle
+getScreenDimensions()
+{
+    GdkRectangle workarea = {0};
+    gdk_monitor_get_workarea(
+            gdk_display_get_primary_monitor(gdk_display_get_default()),
+            &workarea);
+    return workarea;
+}
+
 static void
 createWindow()
 {
@@ -70,27 +80,19 @@ createWindow()
     gtk_widget_show_all(window);
 }
 
-static GdkRectangle
-getScreenDimensions()
-{
-    GdkRectangle workarea = {0};
-    gdk_monitor_get_workarea(
-            gdk_display_get_primary_monitor(gdk_display_get_default()),
-            &workarea);
-    return workarea;
-}
-
 static GtkWidget*
 createLeftPane()
 {
     GtkWidget* grid;
     GtkWidget* text_editing_area;
+    GtkWidget* control_button_area;
 
     grid = gtk_grid_new();
-
     text_editing_area = createTextEditingArea();
+    control_button_area = createControlButtonArea();
 
     gtk_grid_attach(GTK_GRID(grid), text_editing_area, 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID(grid), control_button_area, 0, 2, 1, 1);
 
     return grid;
 }
@@ -108,11 +110,11 @@ createTextEditingArea()
     gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
             GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
     gtk_widget_set_size_request(GTK_WIDGET(scrolled_window),
-            screen_dimensions.width / 2, screen_dimensions.height);
+            screen_dimensions.width / 2, screen_dimensions.height * 2 / 3);
 
     text_view = gtk_text_view_new();
     gtk_widget_set_size_request(GTK_WIDGET(text_view),
-            screen_dimensions.width / 2, screen_dimensions.height);
+            screen_dimensions.width / 2, screen_dimensions.height * 2 / 3);
     gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
     gtk_text_view_set_monospace(GTK_TEXT_VIEW(text_view), TRUE);
 
@@ -125,14 +127,50 @@ static GtkWidget*
 createControlButtonArea()
 {
     GtkWidget* hbox;
+    GtkWidget* button_run;
+    GtkWidget* button_stop;
+    GtkWidget* button_update;
+    GtkWidget* button_save;
+
+    hbox = gtk_hbox_new(TRUE, 10);
+
+    button_run    = gtk_button_new_with_label("Run");
+    button_stop   = gtk_button_new_with_label("Stop");
+    button_update = gtk_button_new_with_label("Update");
+    button_save   = gtk_button_new_with_label("Save");
+
+    gtk_box_pack_start(GTK_BOX(hbox),    button_run, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox),   button_stop, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), button_update, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox),   button_save, TRUE, TRUE, 0);
+
     return hbox;
 }
 
 static GtkWidget*
 createErrorReportingArea()
 {
+    GtkWidget* scrolled_window;
     GtkWidget* text_view;
-    return text_view;
+    GdkRectangle screen_dimensions;
+
+    screen_dimensions = getScreenDimensions();
+
+    scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),
+            GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    gtk_widget_set_size_request(GTK_WIDGET(scrolled_window),
+            screen_dimensions.width / 2, screen_dimensions.height / 6);
+
+    text_view = gtk_text_view_new();
+    gtk_widget_set_size_request(GTK_WIDGET(text_view),
+            screen_dimensions.width / 2, screen_dimensions.height / 6);
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+    gtk_text_view_set_monospace(GTK_TEXT_VIEW(text_view), TRUE);
+
+    gtk_container_add(GTK_CONTAINER(scrolled_window), text_view);
+
+    return scrolled_window;
 }
 
 static GtkWidget*
